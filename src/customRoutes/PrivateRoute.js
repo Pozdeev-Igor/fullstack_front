510eeb886d9af8a3d8eda7531ea18245f206f6ca
Igor <Sketch_90@mail.ru>
2022-07-18 19:01:32 +0300

    redirect to login if not authorized bug fixed

--- src/customRoutes/PrivateRoute.js
+++ src/customRoutes/PrivateRoute.js
@@ -1,12 +1,32 @@
-import React  from 'react';
+import React, { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useLocalState } from '../util/useLocalState';
+import ajax from "../services/fetchService"
 
-const PrivateRoute = ({children}) => {
-
+const PrivateRoute = ({ children }) => {
     const jwt = useLocalState();
+    const jwToken = localStorage.getItem('jwt');
+    const [isLoading, setIsLoading] = useState(true);
+    const [isValid, setIsValid] = useState(null);
 
-    return jwt ? children : <Navigate to="/login" />
+    if (jwToken) {
+
+        ajax(`api/auth/validate?token=${jwToken}`, "get", localStorage.getItem('jwt')).then((isValid) => {
+            setIsValid(isValid);
+            setIsLoading(false);
+            return isValid === true ? children : <Navigate to="/login" />
+        })
+    } else {
+        return <Navigate to="/login" />
+    }
+
+    return isLoading ? (
+        <div>Loading...</div>
+    ) : isValid === true ? (
+        children
+    ) : (
+        <Navigate to="/login" />
+    );
 };
 
 export default PrivateRoute;
