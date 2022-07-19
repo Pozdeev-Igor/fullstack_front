e51823ed877e539ff4a4385e536ec48672fa0d7c
Igor <Sketch_90@mail.ru>
2022-07-19 19:46:22 +0300

    bootstrap addings to dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,51 +1,70 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Card, Button } from 'react-bootstrap';
 import ajax from '../services/fetchService';
-import { useLocalState } from '../util/useLocalState';
 
 const Dashboard = () => {
 
     const [assignments, setAssignments] = useState(null);
 
     useEffect(() => {
-        ajax(   "api/assignments", 
-                "GET", 
-                localStorage.getItem('jwt')).then(
-                    
-                    assignmentsData => {
+        ajax("api/assignments",
+            "GET",
+            localStorage.getItem('jwt')).then(
+
+                assignmentsData => {
                     setAssignments(assignmentsData);
-                    }
-                )
+                }
+            )
     }, []);
 
-    function createAssignment(){
-        ajax(   "/api/assignments", 
-                "POST", 
-                localStorage.getItem('jwt')).then(
-                    
-                    assignment => {
-                    window.location.href = `assignments/${assignment.id}`;
-                    }
-                );
+    function createAssignment() {
+        ajax("/api/assignments",
+            "POST",
+            localStorage.getItem('jwt')).then(
+
+                assignment => {
+                    window.location.href = `/assignments/${assignment.id}`;
+                }
+            );
     }
 
     return (
-        <div style={{margin: "2em"}}>
-            {assignments 
-                        ? 
-            assignments.map((assignment) => 
-                <div key={assignment.id}>
-                    <ol className="list-group list-group">
-                        <li className="list-group-item">
-                            <Link to={`/assignments/${assignment.id}`}>
-                                Assignment ID: {assignment.id}
-                            </Link>
-                        </li>
-                    </ol>
-                </div>) : <></>}
-        
-        <button type="button" className="btn btn-secondary" onClick={() => createAssignment()}>Submit new Assignment</button>
-    </div>
+        <div style={{ margin: "2em" }}>
+            {assignments
+                ?
+                assignments.map((assignment) =>
+                    <div key={assignment.id}>
+
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Body>
+                                <Card.Title>Assignment #{assignment.id}</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">{assignment.status}</Card.Subtitle>
+                                <Card.Text style={{ marginTop: "1em" }}>
+                                    <p>
+                                        <b>Github URL:</b> {assignment.gitHubUrl}
+                                    </p>
+                                    <p>
+                                        <b>Branch:</b> {assignment.branch}
+                                    </p>
+                                </Card.Text>
+                                
+                                <Button onClick={() => {
+                                    window.location.href = `/assignments/${assignment.id}`
+                                }}>Edit</Button>
+                            </Card.Body>
+                        </Card>
+
+                        {/* <ol className="list-group list-group">
+                            <li className="list-group-item">
+                                <Link to={`/assignments/${assignment.id}`}>
+                                    Assignment ID: {assignment.id}
+                                </Link>
+                            </li>
+                        </ol> */}
+                    </div>) : <></>}
+
+            <button type="button" className="btn btn-secondary" onClick={() => createAssignment()}>Submit new Assignment</button>
+        </div>
     );
 };
 
--- src/services/fetchService.js
+++ src/services/fetchService.js
@@ -1,5 +1,5 @@
 
-function ajax(url, requestMethod, jwt, requestBody) {
+async function ajax(url, requestMethod, jwt, requestBody) {
     //const myJwt = jwt;    //может не сработать
 
     const fetchData = {
@@ -17,10 +17,9 @@ function ajax(url, requestMethod, jwt, requestBody) {
         fetchData.body = JSON.stringify(requestBody);
     }
     
-    return fetch(url, fetchData).then((response) => {
-        
-        if (response.status === 200) return response.json();
-    })
+    const response = await fetch(url, fetchData);
+    if (response.status === 200)
+        return response.json();
 }
 
 export default ajax;
