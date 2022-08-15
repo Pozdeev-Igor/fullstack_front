69337dab7f0625b58980897ae1451e7e0f9730b1
Igor <Sketch_90@mail.ru>
2022-08-15 22:10:29 +0300

    AssignmentView added some bootstrap

--- src/pages/AssignmentView.js
+++ src/pages/AssignmentView.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ajax from '../services/fetchService';
+import {Badge, Col, Container, Form, Row} from "react-bootstrap";
 
 
 const AssignmentView = () => {
@@ -41,42 +42,54 @@ const AssignmentView = () => {
     }, [])
 
     return (
-        <div>
-            <h1>Assignment ID: {assigmentId}</h1>
+        <Container className="mt-5">
+            <Row className="d-flex align-items-center">
+                <Col>
+                    <h1>Assignment ID: {assigmentId}</h1>
+                </Col>
+                <Col>
+                    <Badge pill bg="info">
+                        {assignment.status}
+                    </Badge>
+                </Col>
+            </Row>
             {assignment ? 
                 <>
-                    <h2>
-                        Assignment status: {assignment.status}
-                    </h2>
-                    <h3>
-                        GitHub URL: 
-                            <input 
-                                type="URL" 
-                                id="gitHubUrl" 
-                                onChange={(e) => updateAssignment("gitHubUrl", e.target.value)}
-                                value={assignment.gitHubUrl}
-                                /> 
-                    </h3>
-                    <h3>
-                        Branch: 
-                            <input 
-                                type="text" 
-                                id="branch" 
-                                onChange={(e) => updateAssignment("branch", e.target.value)}
-                                value={assignment.branch}
-                                />
-                    </h3>
-                    <button 
+                    <Form.Group as={Row} className="my-4" controlId="formPlaintextEmail">
+                        <Form.Label column sm="3" md="2">
+                            GitHub URL
+                        </Form.Label>
+                        <Col sm="9" md="8" lg="6">
+                            <Form.Control id="gitHubUrl"
+                                          onChange={(e) => updateAssignment("gitHubUrl", e.target.value)}
+                                          value={assignment.gitHubUrl}
+                                          type="url" placeholder="https://github.com/username/repo-name" />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                        <Form.Label column sm="3" md="2">
+                            Branch
+                        </Form.Label>
+                        <Col sm="9" md="8" lg="6">
+                            <Form.Control id="branch"
+                                          onChange={(e) => updateAssignment("branch", e.target.value)}
+                                          value={assignment.branch}
+                                          type="text" placeholder="branch-name" />
+                        </Col>
+                    </Form.Group>
+
+                    <button
                         type="button" 
                         className="btn btn-secondary"
-                        onClick={() => save()}>Submit
+                        onClick={() => save()}>Submit assignment
                     </button>
                 
                 </> : 
                     <>
                     
                     </> }
-        </div>
+        </Container>
     );
 };
 
